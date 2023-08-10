94228b252e39be906adb37b49c28ffac5bad7ea6
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-08-09 21:58:30 -0400

    moved html

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 // Serve the 'index.html' file when accessing the root URL
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/public', 'index.html'));
+  response.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
