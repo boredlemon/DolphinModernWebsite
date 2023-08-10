c6d56afff6847c1364fbb2d1ec7f62a2dfc62823
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-08-09 23:18:16 -0400

    fixed it

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const { port } = require('./config.json');
 
 const app = express();
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, '/public')));
+// Serve static files from the 'images' directory
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Serve static files from the 'styles' directory
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
 
 // Serve the 'index.html' file when accessing the root URL
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './index.html'));
+  response.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
