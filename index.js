55184e8e3a67ae6ec0ca36b41392cf540e461488
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-08-09 20:45:52 -0400

    e

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const { port } = require('./config.json');
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Serve the 'index.html' file when accessing the root URL
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
